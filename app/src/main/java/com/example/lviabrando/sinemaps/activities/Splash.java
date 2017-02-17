e059515b9ba2bc3ef17e1160d0514ce15f559ccc
liviabrandaao <liviaabrandaao@gmail.com>
2017-02-17 15:39:41 -0800

    Initial commit

---
+++ app/src/main/java/com/example/lviabrando/sinemaps/activities/Splash.java
@@ -0,0 +1,29 @@
+package com.example.lviabrando.sinemaps.activities;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.lviabrando.sinemaps.R;
+import com.example.lviabrando.sinemaps.activities.MapsActivity;
+
+public class Splash extends AppCompatActivity implements Runnable{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        Handler handler = new Handler();
+        handler.postDelayed (this, 4000);
+    }
+
+
+
+    @Override
+    public void run() {
+        startActivity(new Intent(this, MapsActivity.class));
+        finish();
+    }
+}
---
+++ app/src/main/java/com/example/lviabrando/sinemaps/activities/network/APIService.java
@@ -0,0 +1,8 @@
+package com.example.lviabrando.sinemaps.activities.network;
+
+/**
+ * Created by Lívia Brandão on 17/02/2017.
+ */
+
+public class APIService {
+}
